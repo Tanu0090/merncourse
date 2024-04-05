5631fe66b5ce73b0ac621a0cf4ba9e40ea92ed47
Tanu0090 <142472414+Tanu0090@users.noreply.github.com>
2024-04-05 09:28:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,177 @@
+
+// function env()
+// {
+//     console.log(message)
+// var message='hello world';
+
+
+// console.log(message)
+// message="else";
+// console.log(message)
+// for(var i=0;i<5;i++)
+// console.log(i)
+// }
+// env();
+// // console.log(i)
+// const message=20;
+// message="else";
+// console.log(message)
+
+
+
+// let n=prompt("enter your number")
+// let fact =1;
+// for(let i=1;i<=n;i++){
+//     fact*=i;
+// }   
+// console.log(fact);
+
+
+
+// let age="abcd";
+// age="cddd";
+// console.log (typeof(age))
+
+
+// let arr=['a','b','c','d']
+// arr[0]='cxxx'
+// console.log(arr)
+
+
+// let str="25";
+// let age=20;
+// let ans=age-str;
+// console.log(ans)
+
+
+// let a="12";
+// let b=12;
+// if(a==b){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }  
+// if(a===b){
+//     console.log("yesagain");
+// }
+// else{
+//     console.log("noagain");
+// }  
+
+
+// const str="tanu";
+// const username="my name is ";
+// const ans=`${username} ${str}`;
+// console.log(ans);
+// function fun(str="upasana"){
+
+//     console.log(str);
+
+// }
+
+// fun("hello")
+
+
+// function sum(a,b){
+//     console.log(sum);
+
+// }
+// sum();
+
+
+//  function print(){
+//     console.log("--")
+
+// }
+ 
+// function declartion (named)
+
+// const a = function print(){
+//     console.log("--")
+
+// }
+// function declartion (anonymous)
+// const b= function (){
+//     console.log("--")
+
+// }
+// arrow function
+
+
+// let a = 2;
+// switch(a){
+//    case 1 :{
+//     console.log("one")
+//    }
+//    case 2 :{
+//     console.log("two")
+//    }
+//    case 3 :{
+//     console.log("three")
+//    }
+// }
+
+// const obj=new Object();
+// obj[1]='one';
+// obj['name']='tanu';
+// obj[10]='ten';
+// console.log(obj);
+
+
+
+// const obj={};
+// obj[1]='one';
+// obj['name']='tanu';
+// obj[10]='ten';
+// console.log(obj);
+
+// const obj={
+//     1:'one',
+//     'firstname':'tanu ',
+//     'lastname':'malik',
+//     'age':20,
+// };
+
+
+// console.log(obj);
+// const x=prompt('please tell the property');
+// console.log(obj[x]);
+
+// let a="20.12";
+// console.log(typeof(a));
+// // let b=parseInt(a);
+// let b=Number(a)
+// console.log(b);
+
+
+// if([])
+// {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// const obj={
+//     "name":"tanu",
+//     "age":21,
+// }
+
+// console.log(obj[age])
+
+
+// function sum(a,b){
+//     console.log(a+b)
+// }
+
+
+
+// let sum = (a,b) =>
+// {
+//     console.log(a+b);
+//     console.log("arrow")
+// }
+
+// sum(2,3)
+
